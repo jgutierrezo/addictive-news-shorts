1b9c9e87acbad2ddc73a0326135022b0eaedf229
jgutierrezo <jgutierrezoa2@gmail.com>
2022-10-16 08:15:55 -0400

    innitial structure

---
+++ app/src/main/java/com/example/addictive_news_shorts/SelectCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.addictive_news_shorts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_category);
+    }
+}
