acfb7f407c241fdb016a0ca5e367d0db404763b1
Chavez <rodrichavezm@gmail.com>
2022-10-19 23:10:01 -0400

    newsPage

---
+++ app/src/main/java/com/example/addictive_news_shorts/newsActivity.java
@@ -0,0 +1,14 @@
+package com.example.addictive_news_shorts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
